44a9d43a528059a998b435d4fc1bb80b86eea776
Armi <armi@lambquil.com>
2024-02-21 11:51:39 +0200

    Added basic moisturizer recipe data structure

--- Project/models.py
+++ Project/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -14,6 +15,8 @@ class Recipe(db.Model):
     description = db.Column(db.Text, nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     phases = db.relationship('Phase', backref='recipe', lazy=True)
+    rating = db.Column(db.Integer, nullable=True)
+    instructions = db.Column(db.Text, nullable=True)
 
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,6 +35,7 @@ class Phase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    note = db.Column(db.Text, nullable=True)
 
 class RecipeIngredientPhase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -39,4 +43,10 @@ class RecipeIngredientPhase(db.Model):
     phase_id = db.Column(db.Integer, db.ForeignKey('phase.id'), nullable=False)
     ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
     quantity = db.Column(db.Float, nullable=False)
-    note = db.Column(db.Text, nullable=True)
+
+class RecipeVersion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    version_number = db.Column(db.Integer, nullable=False)
+    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    recipe = db.relationship('Recipe', backref=db.backref('versions', lazy=True))
